3b7f67bf3c9b0f28c58354747bd789eb34a5b313
shun <shun35960@gmail.com>
2026-03-07 22:51:50 +0900

    ログイン認証をcloudflareAccessに2

--- src/main/java/com/example/MyBlog/Config/SecurityConfig.java
+++ src/main/java/com/example/MyBlog/Config/SecurityConfig.java
@@ -31,25 +31,20 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf
                         .ignoringRequestMatchers("/api/images/**") // 画像API は CSRF 保護を無効化
                 )
-                .formLogin(login -> login
-                        .loginPage("/login") // カスタムログインページのURL
-                        .loginProcessingUrl("/authenticate") // ログイン処理のURL
-                        .defaultSuccessUrl("/Hello") // ログイン成功後のリダイレクト先
-                        .failureUrl("/login?error") // ログイン失敗時のリダイレクト先
-                        .permitAll()
+                .formLogin(AbstractHttpConfigurer::disable
                 ).logout(logout -> logout
-                        .logoutUrl("/logout") // ログアウト処理のURL
-                        .invalidateHttpSession(true) // ログアウト時にセッションを無効化
-                        .deleteCookies("JSESSIONID") // ログアウト時にクッキー削除
-                        .logoutSuccessUrl("/") // ログアウト成功後のリダイレクト先
-                        .permitAll()
-                ).authorizeHttpRequests(auth -> auth
+                .logoutUrl("/logout") // ログアウト処理のURL
+                .invalidateHttpSession(true) // ログアウト時にセッションを無効化
+                .deleteCookies("JSESSIONID") // ログアウト時にクッキー削除
+                .logoutSuccessUrl("/") // ログアウト成功後のリダイレクト先
+                .permitAll()
+        ).authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login", "/register", "/authenticate", "/").permitAll() // ログインページは認証不要
 //                        .requestMatchers("/Hello/**").authenticated()
 //                        .requestMatchers("/api/images/upload").authenticated() // 画像アップロードは認証必要
 //                        .requestMatchers("/api/images/**").permitAll() // 画像取得・削除は誰でも可能
                         .anyRequest().permitAll() // その他のリクエストは認証が必要
-                );
+        );
         return http.build();
     }
 }
