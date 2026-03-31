bca9e1cf30dc05cf0291850981a5c6e29d642f76
shun35960 <shun35960@gmail.com>
2026-03-31 22:13:27 +0900

    cloudflare access有効化済み3(障害対応)

--- src/main/java/com/example/MyBlog/Config/SecurityConfig.java
+++ src/main/java/com/example/MyBlog/Config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //        http
 //                .formLogin(login -> login
