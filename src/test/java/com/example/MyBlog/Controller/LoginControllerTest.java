e4a54b328ee5f1d2b1e2796f5929eb5540c52519
shun <shun35960@gmail.com>
2026-04-20 22:49:35 +0900

    podmanでテスト環境

---
+++ src/test/java/com/example/MyBlog/Controller/LoginControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.MyBlog.Controller;
+
+import com.example.MyBlog.Config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(LoginController.class)
+@Import(SecurityConfig.class)
+class LoginControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void login_GETリクエスト_loginビューが返される() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    void loginError_errorパラメータ付きGETリクエスト_エラーメッセージがモデルに追加される() throws Exception {
+        mockMvc.perform(get("/login").param("error", ""))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andExpect(model().attribute("errormessage", "Invalid username or password."));
+    }
+}
